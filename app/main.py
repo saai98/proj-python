7f200b146d92a604b368e213e5b778490462cbfd
root <root@ip-172-31-36-127.ap-south-1.compute.internal>
2025-04-22 09:51:05 +0000

    ip

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def show_my_ip():
-    return "The server's public IP is: 35.154.122.195"
+def get_server_ip():
+    try:
+        # This returns the actual public IP of your server (not the client)
+        server_ip = requests.get("https://api.ipify.org").text
+        return f"The server's public IP is: 35.154.122.195"
+    except Exception as e:
+        return f"Error getting public IP: {e}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
