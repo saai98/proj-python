334f642b38a24e8a4f5bd16068e7ab48cb1f670d
root <root@ip-172-31-36-127.ap-south-1.compute.internal>
2025-04-22 09:34:12 +0000

    updatecode

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
 from flask import Flask
-import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def get_server_ip():
-    try:
-        ip = requests.get("https://api.ipify.org").text
-        return f"The server's public IP is: {ip}"
-    except Exception as e:
-        return f"Could not fetch public IP. Error: {e}"
+def show_my_ip():
+    return "The server's public IP is: 35.154.122.195"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
